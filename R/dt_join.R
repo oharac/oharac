7ca5660fa2efbed50456b34c60978e3e2da2aff6
Casey O'Hara <casey.c.ohara@gmail.com>
2022-07-31 14:53:16 -0700

    added allow.cartesian to dt_join

--- R/dt_join.R
+++ R/dt_join.R
@@ -14,7 +14,9 @@
 #' @export
 #'
 #' @examples
-dt_join <- function(df1, df2, by, type) {
+dt_join <- function(df1, df2, by, type, allow.cartesian = FALSE) {
+  ### allow.cartesian for when resulting rows is greater than
+  ### nrow(df1) + nrow(df2)
   a <- case_when(type == 'left' ~ c(FALSE, TRUE, FALSE), ### all, all.x, all.y
                  type == 'full' ~ c(TRUE, TRUE, TRUE),
                  type == 'inner' ~ c(FALSE, FALSE, FALSE))
@@ -23,6 +25,8 @@ dt_join <- function(df1, df2, by, type) {
   ### behaves like full join; if all = FTF, behaves like left_join?
   dt1 <- data.table::data.table(df1, key = by)
   dt2 <- data.table::data.table(df2, key = by)
-  dt_full <- merge(dt1, dt2, all = a[1], all.x = a[2], all.y = a[3])
+  dt_full <- data.table::merge(dt1, dt2,
+                               all = a[1], all.x = a[2], all.y = a[3],
+                               allow.cartesian = allow.cartesian)
   return(as.data.frame(dt_full))
 }
